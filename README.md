deb1bb54f7de23c727f6679f60dcab0c495c9ff8
Ashutosh Biswas <ashutoshbw314@gmail.com>
2021-05-02 21:56:21 +0600

    projects page is done for now

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # My Portfolio website 
+
+Built with ❤️ using 11ty(an awesome static site generator).
+
+Live link: <https://ashutoshbiswas.netlify.app/>
---
+++ src/_data/projects.js
@@ -0,0 +1,140 @@
+module.exports = [
+  {
+    name: "Perfect Home",
+    technologies: ["MERN", "Firebase", "Stripe", "React hook form", "Tailwindcss", "Vitejs"],
+    github: "https://github.com/ashutoshbw314/perfect-home-client",
+    website: "https://perfect-home-0.web.app/",
+    features: [
+      {
+        title: "Overview",
+        photo: "../assets/img/projects/perfect-home-1.png",
+        description: "A home service providing company",
+        points: [
+          "On can create an account and then order for a service.",
+          "Credit card payment is handled with stripe.",
+          "Admin users can add new services.",
+          "Users can write a review for a service."
+        ]
+      },
+      {
+        title: "Admin support",
+        photo: "../assets/img/projects/perfect-home-2.png",
+        description: "Admin page",
+        points: [
+          "Admin can set the status of a order.",
+          "Admin delete a service.",
+        ],
+      },
+      {
+        title: "Orders",
+        photo: "../assets/img/projects/perfect-home-3.png",
+        description: "Orders page",
+        points: [
+          "Payment is done with credit card.",
+        ],
+      },
+    ],
+  },
+  {
+    name: "Artpress",
+    technologies: ["MERN", "Firebase", "React hook form", "Tailwindcss", "Vitejs"],
+    github: "https://github.com/ashutoshbw314/artpress-client",
+    website: "https://art-press.web.app/",
+    features: [
+      {
+        title: "Overview",
+        photo: "../assets/img/projects/artpress-1.png",
+        description: "A site artists and art lovers where artists can make living by selling their artworks.",
+        points: [
+          "Artists can sell artworks.",
+          "Anyone can buy artworks.",
+        ]
+      },
+      {
+        title: "Manage Artworks",
+        photo: "../assets/img/projects/artpress-2.png",
+        description: "Admin can manage artworks.",
+        points: [
+          "Each artworks detail information is shown.",
+          "An item can be deleted",
+        ],
+      },
+      {
+        title: "Add artwork",
+        photo: "../assets/img/projects/artpress-3.png",
+        description: "Here artists can upload their art's photo and information.",
+        points: [
+          "An user can set price of the product.",
+        ],
+      },
+    ],
+  },
+  {
+    name: "Let's Ride",
+    technologies: ["React", "React leaflet", "Firebase", "React hook form", "Tailwindcss", "Vitejs"],
+    github: "https://github.com/ashutoshbw314/perfect-home-client",
+    website: "https://perfect-home-0.web.app/",
+    features: [
+      {
+        title: "Overview",
+        photo: "../assets/img/projects/lets-ride-1.png",
+        description: "A travelling website.",
+        points: [
+          "Anyone can order for ride.",
+          "One can select a vehicle of choice.",
+        ]
+      },
+      {
+        title: "Interactive map",
+        photo: "../assets/img/projects/lets-ride-2.png",
+        description: "User can see routes using interactive map.",
+        points: [],
+      },
+    ],
+  },
+  {
+    name: "Legendary Leagues",
+    technologies: ["React", "Material UI", "Vitejs"],
+    github: "#",
+    website: "https://legendary-leagues.netlify.app/",
+    features: [
+      {
+        title: "Overview",
+        photo: "../assets/img/projects/legendary-leagues-1.png",
+        description: "An compilation of famous sport leagues around the world.",
+        points: [
+          "On can select a league to see more info.",
+          "All data are came from a API."
+        ]
+      },
+      {
+        title: "Detail League info",
+        photo: "../assets/img/projects/legendary-leagues-2.png",
+        description: "Here you can see detail information about the selected league.",
+        points: [],
+      },
+    ],
+  },
+  {
+    name: "Meal Matcher",
+    technologies: ["JavaScript", "HTML", "CSS", "themealdb api"],
+    github: "https://github.com/ashutoshbw314/meal-matcher",
+    website: "https://ashutoshbw314.github.io/meal-matcher/index.html",
+    features: [
+      {
+        title: "Overview",
+        photo: "../assets/img/projects/meal-matcher-1.png",
+        description: "An compilation of meals with their recipes.",
+        points: [
+          "On can select a meal to see it's recipe",
+        ]
+      },
+      {
+        title: "The recipe",
+        photo: "../assets/img/projects/meal-matcher-2.png",
+        description: "Here one can see the recipe.",
+        points: [],
+      },
+    ],
+  },
+]; 
